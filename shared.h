4851d7024f7e1a50ad3e2d5b8f14bb779ab10156
Joyal <joyal.edu@gmail.com>
2021-03-10 02:12:45 +0530

    Added shared

--- shared.h
+++ shared.h
@@ -47,5 +47,36 @@
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
-#include "./shared.h"
-DECLARATIVE_EXAMPLE_MAIN(dialcontrol)
+#include <QDir>
+#include <QGuiApplication>
+#include <QQmlEngine>
+#include <QQmlFileSelector>
+#include <QQuickView> //Not using QQmlApplicationEngine because many examples don't have a Window{}
+#define DECLARATIVE_EXAMPLE_MAIN(NAME) int main(int argc, char* argv[]) \
+{\
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);\
+    QGuiApplication app(argc,argv);\
+    app.setOrganizationName("QtProject");\
+    app.setOrganizationDomain("qt-project.org");\
+    app.setApplicationName(QFileInfo(app.applicationFilePath()).baseName());\
+    QQuickView view;\
+    if (qgetenv("QT_QUICK_CORE_PROFILE").toInt()) {\
+        QSurfaceFormat f = view.format();\
+        f.setProfile(QSurfaceFormat::CoreProfile);\
+        f.setVersion(4, 4);\
+        view.setFormat(f);\
+    }\
+    if (qgetenv("QT_QUICK_MULTISAMPLE").toInt()) {\
+        QSurfaceFormat f = view.format();\
+        f.setSamples(4);\
+        view.setFormat(f);\
+    }\
+    view.connect(view.engine(), &QQmlEngine::quit, &app, &QCoreApplication::quit);\
+    new QQmlFileSelector(view.engine(), &view);\
+    view.setSource(QUrl("qrc:///" #NAME ".qml")); \
+    if (view.status() == QQuickView::Error)\
+        return -1;\
+    view.setResizeMode(QQuickView::SizeRootObjectToView);\
+    view.show();\
+    return app.exec();\
+}
